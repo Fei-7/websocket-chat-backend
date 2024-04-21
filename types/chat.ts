0c1049d0a6444737e9d50759d3af6b19072cb9ed
Fei-7 <supachokang@gmail.com>
2024-04-21 13:30:27 +0700

    chore : refactor types/chat.ts

--- types/chat.ts
+++ types/chat.ts
@@ -2,16 +2,16 @@ export type toServerTextMessage = {
     text: string
 };
 
+export type toServerImageMessage = {
+    type: string,
+    size: number,
+    buffer: Buffer
+};
+
 export type toClientMessage = {
     id: string;
     userId: string;
     createdAt: string;
     content: string;
     isImage: boolean;
-};
-
-export type toServerImageMessage = {
-    type: string,
-    size: number,
-    buffer: Buffer
 };
