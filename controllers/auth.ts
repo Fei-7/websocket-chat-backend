56a5be091d339d0af6000857d57a162f3b55e4a9
Fei-7 <supachokang@gmail.com>
2024-04-22 16:46:31 +0700

    fix: cookie problem

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -88,7 +88,7 @@ function sendTokenResponse(user: user, statusCode: number, res: Response) {
         expires: new Date(
             Date.now() + jwtCookieExpire * 24 * 60 * 60 * 1000,
         ),
-        httpOnly: true,
+        sameSite: false
     };
     res.status(statusCode).cookie("token", token, options).json({
         success: true,
